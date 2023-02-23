08d1fd114b25e8384e437026b2a516d34f0f29f5
Eujung Kim <eujung.kim@gmail.com>
2023-02-24 01:59:56 +0900

    Update JavaProcess.java

--- src/prog22b/sub4/JavaProcess.java
+++ src/prog22b/sub4/JavaProcess.java
@@ -1,101 +1,36 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
-import org.eclipse.jetty.client.HttpClient;
-import org.eclipse.jetty.client.util.StringContentProvider;
-import org.eclipse.jetty.http.HttpMethod;
+public final class JavaProcess {
 
-import com.google.gson.Gson;
+    private JavaProcess() {}        
 
-class ProcessResult {
-	public List<String> resultList;
-	long updateTime;
-}
-public class MyProcess {
-	public static void main(String[] args) throws Exception {
-		// processNo, batchSize, output, intputlist 
-		List<String> resultList = Collections.synchronizedList(new ArrayList<>());
-		ProcessResult processResult = new ProcessResult();
-		processResult.resultList = resultList;
-		processResult.updateTime = -1;
-		
-		int outputQueueBatchSize = Integer.parseInt(args[1]);
-		String outputQueueURI = args[2];
-		
-		Map<Integer, WorkerThread> servers = new HashMap<>();
-		for (int i = 3; i< args.length; i++) {
-			String value = args[i];
-			String[] tmp = value.split("#"); // threadNo # workerNo # inputURI
-			int threadNo = Integer.parseInt(tmp[0]);
-			int workerNo = Integer.parseInt(tmp[1]);
-			String inputURI = tmp[2];
+    public static int exec(Class klass, List<String> args) throws IOException,
+                                               InterruptedException {
+        String javaHome = System.getProperty("java.home");
+        String javaBin = javaHome +
+                File.separator + "bin" +
+                File.separator + "java";
+        String classpath = System.getProperty("java.class.path");
+        String className = klass.getName();
 
-			if (!servers.containsKey(threadNo)) {
-				servers.put(threadNo, new WorkerThread(threadNo, outputQueueBatchSize, outputQueueURI, resultList, processResult));
-			}
-			servers.get(threadNo).workerMap.put(workerNo, new Worker(workerNo));
-			servers.get(threadNo).inputQueueURIMap.put(workerNo, inputURI);				
-		}
-		
-		for (int threadNo : servers.keySet()) {
-			Thread thread = new Thread(servers.get(threadNo));
-			thread.start();
-		}
-		
-		
-		Thread timeChecker = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				Gson gson = new Gson();
-				HttpClient httpClient = new HttpClient();
-				try {
-					httpClient.start();
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-				}
-				while (true) {
-					try {
-						Thread.sleep(100);
+        List<String> command = new LinkedList<String>();
+        command.add(javaBin);
+        command.add("-cp");
+        command.add(classpath);
+        command.add(className);
+        if (args != null) {
+            command.addAll(args);
+        }
+        ProcessBuilder builder = new ProcessBuilder(command);
 
-						if (resultList.size() > 0 && System.currentTimeMillis() - processResult.updateTime >= 2000) {
-							String content = gson.toJson(new OutputQueueData(resultList));
-							
-							httpClient.newRequest(outputQueueURI).method(HttpMethod.POST)
-							.content(new StringContentProvider(content)).send();
-							resultList.clear();
-							processResult.updateTime = -1;
-						}
+        Process process = builder.inheritIO().start();
+        process.waitFor();
+        return process.exitValue();
+    }
 
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				
-			}
-			
-		});
-		timeChecker.start();
-		/*
-		FileWriter writer = new FileWriter("c:/test/" + args[0]  + ".txt");
-		PrintWriter pw = new PrintWriter(writer);
-		for (String line : args) {
-			pw.println(line);
-		}
-		pw.close();
-		writer.close();
-		
-		
-		int batchSize = Integer.parseInt(args[1]);
-		
-		Map<String, Worker> map = new HashMap<>();
-		*/
-		// java com.lgcns.test.MyProcess
-		
-		//File file = new File("c:/test/" + System.currentTimeMillis());
-		//file.mkdir();
-	}
+    
+    // use : JavaProcess.exec(MyProcess.class, ins); // ins : List<String>
 }
