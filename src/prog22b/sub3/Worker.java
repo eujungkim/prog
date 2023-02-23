9adee5c7cc65116aeb549a7674b30d4d10d2e46b
Eujung Kim <eujung.kim@gmail.com>
2023-02-24 01:57:35 +0900

    Create Worker.java

---
+++ src/prog22b/sub3/Worker.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Worker extends AbstractWorker {
+	
+	public Worker(int queueNo) {
+		super(queueNo);
+	}
+	
+	public void removeExpiredStoreItems(long timestamp, List<String> store) {
+		
+		for (int i = store.size()-1; i >=0; i--) {
+			String value = store.get(i);
+			String[] temp = value.split("#");
+			long ts = Long.parseLong(temp[0]);
+			if (timestamp - ts > 3000) {
+				store.remove(i);
+			}
+		}
+
+	}
+}
